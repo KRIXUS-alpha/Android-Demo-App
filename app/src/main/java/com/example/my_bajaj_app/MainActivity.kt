3ce489bcaf69ea96aec82b62ea75862a4c978da8
KRIXUS-alpha <kartikrockson1@gmail.com>
2022-01-27 10:30:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/my_bajaj_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.my_bajaj_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View) {
+        Toast.makeText(this, "this is a toast", Toast.LENGTH_SHORT).show()
+    }
+}
